463cc21ddae90da0df6cba1fea671bea9f94cca2
mross1080 <mattross1080@gmail.com>
2016-09-14 14:18:55 -0400

    adds user standardization functionality and display user page

--- domain/src/main/java/io/mross/phonebook/domain/User.java
+++ domain/src/main/java/io/mross/phonebook/domain/User.java
@@ -4,25 +4,19 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.gemfire.mapping.Region;
 
-@Region("Users")
-public class User {
+@Region("Users") public class User {
 
-
-  @Id
-  private String id;
+  @Id private String id;
   private String name;
   private String phoneNumber;
   private String location;
-  
 
-
-  @PersistenceConstructor
-  public User() {
+  @PersistenceConstructor public User() {
 
   }
 
-  @PersistenceConstructor
-  public User(String id, String name, String phoneNumber, String location) {
+  @PersistenceConstructor public User(String id, String name,
+      String phoneNumber, String location) {
     this.id = id;
     this.name = name;
     this.phoneNumber = phoneNumber;
@@ -69,4 +63,33 @@ public class User {
         ", location='" + location + '\'' +
         '}';
   }
+
+  public void standardizePhoneNumber(char delimeter) {
+
+    System.out.println(phoneNumber + " " + phoneNumber.length());
+    if (phoneNumber.length() == 12) {
+      char oldDelimeter = phoneNumber.charAt(3);
+
+      phoneNumber = phoneNumber.replace(oldDelimeter, delimeter);
+
+    } else {
+      if (phoneNumber.length() == 11) {
+        phoneNumber = phoneNumber.substring(1, phoneNumber.length());
+      } else if (phoneNumber.length() == 14) {
+        phoneNumber = phoneNumber.substring(2, phoneNumber.length());
+        char oldDelimeter = phoneNumber.charAt(3);
+        if (oldDelimeter == delimeter) {
+          return;
+        }
+      }
+      String areaCode = phoneNumber.substring(0, 3);
+      String firstIdentifier = phoneNumber.substring(3, 6);
+      String secondIdentifier = phoneNumber.substring(6, 10);
+
+      phoneNumber =
+          areaCode + delimeter + firstIdentifier + delimeter + secondIdentifier;
+    }
+
+  }
+
 }
---
+++ domain/src/test/java/io/mross/UserTests.java
@@ -0,0 +1,61 @@
+package io.mross;
+
+import io.mross.phonebook.domain.User;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by mross on 9/14/16.
+ */
+public class UserTests {
+
+  @Test
+  public void testStandardizePhoneNumnber() {
+    User user = new User("1","Danny Brown","6163452212","Seattle");
+    user.standardizePhoneNumber('-');
+    String phoneNumber = user.getPhoneNumber();
+
+    assertEquals("616-345-2212",phoneNumber);
+
+    user.setPhoneNumber("5411120989");
+    phoneNumber = user.getPhoneNumber();
+
+    assertEquals("541-112-0989", phoneNumber);
+
+  }
+
+  @Test
+  public void testDifferentDelimeter() {
+    User user = new User("1","Danny Brown","6163452212","Seattle");
+    user.standardizePhoneNumber('-');
+    String phoneNumber = user.getPhoneNumber();
+
+    assertEquals("616-345-2212",phoneNumber);
+
+    user.standardizePhoneNumber('/');
+    phoneNumber = user.getPhoneNumber();
+    assertEquals("616/345/2212",phoneNumber);
+
+    user.standardizePhoneNumber('*');
+    phoneNumber = user.getPhoneNumber();
+    assertEquals("616*345*2212",phoneNumber);
+  }
+
+  @Test
+  public void testRemoveCountryCode() {
+    User user = new User("1","Danny Brown","18597542123","Seattle");
+    user.standardizePhoneNumber('-');
+    String phoneNumber = user.getPhoneNumber();
+
+    assertEquals("859-754-2123",phoneNumber);
+
+    user.setPhoneNumber("1-859-754-2123");
+    user.standardizePhoneNumber('-');
+    phoneNumber = user.getPhoneNumber();
+
+    assertEquals("859-754-2123",phoneNumber);
+
+  }
+
+}
